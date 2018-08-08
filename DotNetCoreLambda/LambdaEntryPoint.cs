b993f05ca43a7310a5872f9aca88c4e4d8289bce
JP Dillingham <jpdillingham@gmail.com>
2018-08-07 20:04:26 -0500

    clean up LambdaEntryPoint

--- DotNetCoreLambda/LambdaEntryPoint.cs
+++ DotNetCoreLambda/LambdaEntryPoint.cs
@@ -1,17 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Net;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Hosting;
-using System.IO;
+﻿using Microsoft.AspNetCore.Hosting;
+using Amazon.Lambda.AspNetCoreServer;
 
 namespace DotNetCoreLambda
 {
-    public class LambdaEntryPoint : Amazon.Lambda.AspNetCoreServer.APIGatewayProxyFunction
+    public class LambdaEntryPoint : APIGatewayProxyFunction
     {
-        /// <summary> The builder has configuration, logging and Amazon API Gateway already configured. The startup class needs to
-        /// be configured in this method using the UseStartup<>() method. </summary> <param name="builder"></param>
         protected override void Init(IWebHostBuilder builder)
         {
             builder
